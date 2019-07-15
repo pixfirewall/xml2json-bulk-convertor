da2717e04a605e60ff3ad511c6f8c9e1c845c162
Amir Hodaee <amir.hodaee@goquo.com>
2019-07-16 01:57:33 +0800

    revised

--- lib.js
+++ lib.js
@@ -5,90 +5,102 @@ const Queue = require('better-queue');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
+const _cluster 		= new WeakMap(),
+			_readDir 		= new WeakMap(),
+			_preCluster = new WeakMap(),
+			_x2j 				= new WeakMap(),
+			_x2jSmal 		= new WeakMap();
+			_xmlPath 		= new WeakMap(),
+			_jsonPath		= new WeakMap(),
+			_concurrent	= new WeakMap(),
+			_fileNames	= new WeakMap(),
+			_classMap		= new WeakMap();
+
+
 class Convertor {
 	constructor(opt) {
-		this.xmlPath = opt.input || 'xml';
-		this.jsonPath = opt.output || 'json';
-		this.concurrent = opt.concurrent || 1;
+		if(!opt) opt = {};
+		_xmlPath.set(this, opt.input || 'xml');
+		_jsonPath.set(this, opt.output || 'json');
+		_concurrent.set(this, opt.concurrent || 100);
+
+		_cluster.set(this, () => {
+			if (cluster.isMaster) {
+				for (let i = 0; i < numCPUs; i++) {
+					const worker = cluster.fork()
+					worker.send(_classMap.get(this)[i])
+				}
+			} 
+			else {
+				process.on('message', msg => {
+					_x2j.get(this)(msg)
+				})
+			}
+		})
+
+		_readDir.set(this, () => {
+			const pathI = path.join(__dirname, _xmlPath.get(this))
+			return fs.readdirSync(pathI).filter(i => /\.xml/.test(i));
+		})
+
+		_preCluster.set(this, () => {
+			const fileCount = _fileNames.get(this).length;
+				const loopCicle = (Math.floor(fileCount/numCPUs)) - 1
+				const arr = [];
+				for(let j = 0, i = 1 ; i < numCPUs ; j += (loopCicle+1) , i++){
+					arr[i-1] = [j, (j+loopCicle)]
+					if(i === numCPUs - 1) arr[i] = [(j+loopCicle+1), (fileCount-1)]
+				}
+				return arr
+		})
+
+		_x2j.set(this, i => {
+			const q = new Queue((input, cb) => {
+				let pathO = path.join(__dirname, _jsonPath.get(this), input.replace('.xml', '.json'))
+				const pathI = path.join(__dirname, _xmlPath.get(this), input)
+				const jsonContent = convert.xml2json(fs.readFileSync(pathI), {compact: true, spaces: 4});
+				if(fs.existsSync(pathO)) pathO = path.join(__dirname, _jsonPath.get(this), input.replace('.xml', `_${Math.ceil(Math.random()*10)}.json`))
+				fs.writeFile(pathO, jsonContent, (e) => {
+					if (e) console.log(`${input} Error.`)
+					else console.log(`${input} Done.`)
+				});
+				cb(null, result);
+			}, { concurrent: _concurrent.get(this) })
+
+			for(let j = i[0] ; j <= i[1] ; j++) {
+				q.push(_fileNames.get(this)[j])
+			}
+		})
+
+		_x2jSmal.set(this, () => {
+			_fileNames.get(this).forEach(input => {
+				let pathO = path.join(__dirname, _jsonPath.get(this), input.replace('.xml', '.json'))
+				const pathI = path.join(__dirname, _xmlPath.get(this), input)
+				const jsonContent = convert.xml2json(fs.readFileSync(pathI), {compact: true, spaces: 4});
+				fs.writeFile(pathO, jsonContent, () => {
+					console.log(`${input} Done.`)
+				});
+			})
+		})
 	}
 
 	start() {
-		fs.stat(path.join(__dirname, this.xmlPath), (e) => {
+		fs.stat(path.join(__dirname, _xmlPath.get(this)), (e) => {
 			if(e) return console.log('input dir not exist.')
-			this.fileNames = this._readDir()
+			_fileNames.set(this, _readDir.get(this)())
 			try {
-				fs.statSync(path.join(__dirname, this.jsonPath));
-				if(this.fileNames.length < numCPUs) this._x2jSmal();
+				fs.statSync(path.join(__dirname, _jsonPath.get(this)));
+				if(_fileNames.get(this).length < numCPUs) _x2jSmal.get(this)();
 				else {
-					this.classMap = this._preCluster();
-					this._cluster();
+					_classMap.set(this, _preCluster.get(this)())
+					_cluster.get(this)();
 				}
-	
+
 			} catch(e) {
 				console.log('output path not exist.')
 			}
 		})
 	}
-
-	_cluster() {
-		if (cluster.isMaster) {
-			for (let i = 0; i < numCPUs; i++) {
-				const worker = cluster.fork()
-				worker.send(this.classMap[i])
-			}
-		} 
-		else {
-			process.on('message', msg => {
-				this._x2j(msg)
-			})
-		}
-	}
-
-	_readDir() {
-		const pathI = path.join(__dirname, this.xmlPath)
-		return fs.readdirSync(pathI).filter(i => /\.xml/.test(i));
-	}
-
-	_preCluster() {
-		const fileCount = this.fileNames.length;
-			const loopCicle = (Math.floor(fileCount/numCPUs)) - 1
-			const arr = [];
-			for(let j = 0, i = 1 ; i < numCPUs ; j += (loopCicle+1) , i++){
-				arr[i-1] = [j, (j+loopCicle)]
-				if(i === numCPUs - 1) arr[i] = [(j+loopCicle+1), (fileCount-1)]
-			}
-			return arr
-	}
-
-	_x2j(i) {
-		const q = new Queue((input, cb) => {
-			let pathO = path.join(__dirname, this.jsonPath, input.replace('.xml', '.json'))
-			const pathI = path.join(__dirname, this.xmlPath, input)
-			const jsonContent = convert.xml2json(fs.readFileSync(pathI), {compact: true, spaces: 4});
-			if(fs.existsSync(pathO)) pathO = path.join(__dirname, this.jsonPath, input.replace('.xml', `_${Math.ceil(Math.random()*10)}.json`))
-			fs.writeFile(pathO, jsonContent, (e) => {
-				if (e) console.log(`${input} Error.`)
-				else console.log(`${input} Done.`)
-			});
-			cb(null, result);
-		}, { concurrent: this.concurrent })
-
-		for(let j = i[0] ; j <= i[1] ; j++) {
-			q.push(this.fileNames[j])
-		}
-	}
-
-	_x2jSmal() {
-		this.fileNames.forEach(input => {
-			let pathO = path.join(__dirname, this.jsonPath, input.replace('.xml', '.json'))
-			const pathI = path.join(__dirname, this.xmlPath, input)
-			const jsonContent = convert.xml2json(fs.readFileSync(pathI), {compact: true, spaces: 4});
-			fs.writeFile(pathO, jsonContent, () => {
-				console.log(`${input} Done.`)
-			});
-		})
-	}
-	
 }
 
 module.exports = Convertor;
